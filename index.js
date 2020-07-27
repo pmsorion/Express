c30575a456f24d211e8012b7f41bac4931c203e9
Francisco Garcia <garcifran@hotmail.com>
2020-07-27 09:06:38 -0500

    hello world

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', function(req, res, next) {
-    res.send("hello world");
+    res.send({ hello: "world" });
 })
 
 const server = app.listen(8000, () => {
