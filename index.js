643afcfa727477e6de8d1405be88dbedcef80f3a
Francisco Garcia <garcifran@hotmail.com>
2020-07-26 20:21:06 -0500

    primer commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+app.get('/', function(req, res, next) {
+    res.send("hello world");
+})
+
+const server = app.listen(8000, () => {
+    console.log(`Listening http://localhost:${server.address().port}`);
+});
+
